79669923e599674abaf5e9815ce441154d1fdd12
Christian Burkhart <burkhart.christian@gmail.com>
2020-04-29 09:16:54 +0200

    feat: minor changes

--- doczrc.js
+++ doczrc.js
@@ -3,8 +3,8 @@ import * as colors from './src/utils/colors'
 export default {
   title: 'E-Learning Dokumentation EZW Freiburg',
   base: '/docs',
-  ignore: ['**/blog/**', 'readme.md'],
-  menu: ['Getting Started', 'Components', "Tools", "Videos", "Umfragen"],
+  ignore: ['**/blog/**', 'Readme.md'],
+  menu: ['Getting Started', "Tools", "Videos", "Umfragen"],
   themeConfig: {
     colors: {
       primary: colors.primary,
